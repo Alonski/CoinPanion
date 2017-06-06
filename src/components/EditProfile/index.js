68c7b0d0d769b5e5c98820daa1340d13c7633fc5
Rahul Sethuram <rksethuram9@gmail.com>
2017-06-05 20:17:41 -0700

    Edit profile functionality

--- src/components/EditProfile/index.js
+++ src/components/EditProfile/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import TextField from 'material-ui/TextField'
-import DropDownMenu from 'material-ui/DropDownMenu'
 import MenuItem from 'material-ui/MenuItem'
 import Avatar from 'material-ui/Avatar'
 import RaisedButton from 'material-ui/RaisedButton'
+import SelectField from 'material-ui/SelectField'
+import * as firebase from 'firebase'
+import Snackbar from 'material-ui/Snackbar'
 
 const Main = styled.div`
   display: flex;
@@ -39,40 +41,97 @@ const categories = [
 ]
 
 class CategoryDropDown extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { value: 1 }
-  }
-
-  handleChange = (event, index, value) => this.setState({ value })
-
   render() {
     return (
       <div>
-        <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-          {categories.map((category, index) => <MenuItem value={index + 1} primaryText={category} />)}
-        </DropDownMenu>
+        <SelectField
+          floatingLabelText="Category"
+          value={this.props.value}
+          onChange={this.props.onChange}
+          maxHeight={200}
+        >
+          {categories.map((category, index) => <MenuItem key={index} value={category} primaryText={category} />)}
+        </SelectField>
       </div>
     )
   }
 }
 
 class EditProfile extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fullNameError: false,
+      category: 'None',
+      fullName: '',
+      biography: '',
+      openSnackbar: false,
+      snackbarMessage: ''
+    }
+  }
+
+  handleSave = event => {
+    const myAddress = this.props.addresses[0]
+    const userProfile = {
+      fullName: this.state.fullName,
+      category: this.state.category,
+      biography: this.state.biography,
+      address: myAddress // get address from app state so there is no danger of being manipulated from front-end
+    }
+    console.log(userProfile)
+    if (this.state.fullName) {
+      firebase.database().ref('users/' + myAddress).set(userProfile).then((success, error) => {
+        if (error) {
+          console.error(error)
+          this.setState({ openSnackbar: true, snackbarMessage: 'Error: Profile not saved' })
+        } else {
+          this.setState({ openSnackbar: true, snackbarMessage: 'Profile saved' })
+        }
+      })
+    } else {
+      this.setState({ fullNameError: 'Full name is required' })
+    }
+  }
+
+  handleCategoryChange = (event, index, value) => {
+    this.setState({ category: value })
+  }
+
+  handleFullNameChange = (event, newValue) => {
+    if (!newValue) {
+      this.setState({ fullNameError: 'Full name is required' })
+    } else {
+      this.setState({ fullNameError: false })
+      this.setState({ fullName: newValue })
+    }
+  }
+
+  handleBiographyChange = (event, newValue) => {
+    this.setState({ biography: newValue })
+  }
+
   render() {
     return (
       <Main>
         <FormContainer>
           <h1>Edit My Profile</h1>
           <Avatar src="avatar.jpeg" size={250} />
-          <TextField floatingLabelText="Full Name" />
-          <CategoryDropDown />
-          <TextField floatingLabelText="Biography" />
           <TextField
-            disabled={true}
-            defaultValue="0x02840384058048504850490594904590"
-            floatingLabelText="Ethereum Address"
+            floatingLabelText="Full Name"
+            onChange={this.handleFullNameChange}
+            errorText={this.state.fullNameError}
           />
-          <RaisedButton label="Save Profile" primary={true} />
+          <CategoryDropDown onChange={this.handleCategoryChange} value={this.state.category} />
+          <TextField floatingLabelText="Biography" onChange={this.handleBiographyChange} />
+          <span>Ethereum Address</span>
+          <span>{this.props.addresses[0]}</span>
+          <RaisedButton label="Save Profile" primary={true} onTouchTap={this.handleSave} />
+          <Snackbar
+            open={this.state.openSnackbar}
+            message={this.state.snackbarMessage}
+            autoHideDuration={4000}
+            onRequestClose={this.handleRequestClose}
+          />
         </FormContainer>
       </Main>
     )
