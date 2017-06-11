1e3bea0970da29821ab42c5e65286a50cc678d7e
Tim Reznich <tim.reznich@gmail.com>
2017-06-11 16:23:06 +0300

    Dashboard coinings

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -8,25 +8,35 @@ export function getUser(user) {
     const coiningsRef = querybase.ref(databaseRefCoinings, [])
     const databaseRefUsers = firebase.database().ref().child('users')
     const usersRef = querybase.ref(databaseRefUsers, [])
+
+    const coineeObject = {}
     const coineeIsMeSnap = await coiningsRef.where({ coinee: user.eth_address }).once('value')
-    const coinerIsMeSnap = await coiningsRef.where({ coiner: user.eth_address }).once('value')
     const coineeIsMePromise = Object.values(coineeIsMeSnap.val() || {}).map(coining => {
+      coineeObject[coining.coiner.toLowerCase()] = coining
       return usersRef.where({ eth_address: coining.coiner.toLowerCase() }).once('value')
     })
     const responseCoineeIsMe = await Promise.all(coineeIsMePromise)
     user.coinedBy = responseCoineeIsMe.map(res => {
       const [user] = Object.values(res.val())
+      console.log(user, coineeObject)
+      console.log(coinerObject, user.eth_address)
+      user.coining = coineeObject[user.eth_address]
       return user
     })
+
+    const coinerObject = {}
+    const coinerIsMeSnap = await coiningsRef.where({ coiner: user.eth_address }).once('value')
     const coinerIsMePromise = Object.values(coinerIsMeSnap.val() || {}).map(coining => {
-      console.log(coining.coinee, usersRef.where({ eth_address: coining.coinee.toLowerCase() }).once('value'))
+      coinerObject[coining.coinee.toLowerCase()] = coining
       return usersRef.where({ eth_address: coining.coinee.toLowerCase() }).once('value')
     })
     const responseCoinerIsMe = await Promise.all(coinerIsMePromise)
     user.coinedByMe = responseCoinerIsMe.map(res => {
       const [user] = Object.values(res.val())
+      user.coining = coinerObject[user.eth_address]
       return user
     })
+
     dispatch({ type: actionTypes.GET_USER, payload: user })
   }
 }
